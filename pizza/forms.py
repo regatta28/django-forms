7c6ee954b897de154fe072b40bb9961769ffa8cb
Rebecca Stevens <rsbjshan@gmail.com>
2020-06-17 13:41:49 +0100

    In forms.py declared size in pizzaform class to be a multiplechoicefield using the imported size objects from models.py and gave it widget of radioselect

--- pizza/forms.py
+++ pizza/forms.py
@@ -1,17 +1,19 @@
 from django import forms
-from .models import Pizza
+from .models import Pizza, Size
 
 
 
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label='Topping 1', max_length = 100, widget=forms.Textarea)
-    topping2 = forms.CharField(label='Topping 2', max_length = 100)
-    # toppings = forms.MultipleChoiceField(choices = [('pep', 'Pepperoni'), ('cheese','Cheese'), ('olives', 'Olives')], widget=forms.CheckboxSelectMultiple)
-    size = forms.ChoiceField(label ='Size', choices=[('Small','Small'),('Medium','Medium'),('Large','Large')])
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='Topping 1', max_length = 100, widget=forms.Textarea)
+#     topping2 = forms.CharField(label='Topping 2', max_length = 100)
+#     # toppings = forms.MultipleChoiceField(choices = [('pep', 'Pepperoni'), ('cheese','Cheese'), ('olives', 'Olives')], widget=forms.CheckboxSelectMultiple)
+#     size = forms.ChoiceField(label ='Size', choices=[('Small','Small'),('Medium','Medium'),('Large','Large')])
 
 
-# class PizzaForm(forms.ModelForm):
-#     class Meta:
-#         model = Pizza  
-#         fields = ['topping1', 'topping2', 'size']
-#         labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
+class PizzaForm(forms.ModelForm):
+
+    size = forms.ModelChoiceField(queryset=Size.objects, empty_label=None, widget=forms.RadioSelect)
+    class Meta:
+        model = Pizza  
+        fields = ['topping1', 'topping2', 'size']
+        labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
